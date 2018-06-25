b81c23ee1c66a6b17ad638e354564c93912d80ca
53854 <34171367+53854@users.noreply.github.com>
2018-06-25 17:49:52 +0200

    Update gameOver.js

--- js/States/gameOver.js
+++ js/States/gameOver.js
@@ -5,13 +5,13 @@ var gameOverState = {
         if (!menuMusic.isPlaying){  menuMusic.play('',0,0.05,true); }
         else {menuMusic.volume = 0.05}
 
-        var gameOverZombie = game.add.sprite(game.world.centerX-100, game.world.centerY , "zombieload");
+        var gameOverZombie = game.add.sprite(game.world.centerX-150, game.world.centerY , "zombieload");
         gameOverZombie.anchor.setTo(0.5);
         gameOverZombie.scale.setTo(0.2, 0.2);
 
-        var gameOverText = game.add.text(game.world.centerX + 20, game.world.centerY, "RIP", {font: "50px Agency FB", fill: "#ffffff"});
+        var gameOverText = game.add.text(game.world.centerX + 50, game.world.centerY, "RIP", {font: "50px Agency FB", fill: "#ffffff"});
 
-        var backText = game.add.text( game.world.centerX, game.height - 100, "press space to get back", {fill: "#fff"});
+        var backText = game.add.text( game.world.centerX, game.height - 100, "press space to get back", {font: "25px Agency FB", fill: "#fff"});
 
         spaceKey.onDown.addOnce(this.restart, this);
     },
