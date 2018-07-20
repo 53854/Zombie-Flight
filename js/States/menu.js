e5fc614d3f062605a36e5c47a5838c934adf0b27
53854 <34171367+53854@users.noreply.github.com>
2018-07-20 13:29:13 +0200

    Update menu.js

--- js/States/menu.js
+++ js/States/menu.js
@@ -7,7 +7,7 @@ var menuState ={
     create: function(){
         
         //Music
-        if (!menuMusic.isPlaying){  menuMusic.play('',0,0.05,true); }
+        if (!menuMusic.isPlaying){  menuMusic.play('',0,0.3,true); }
         else {menuMusic.volume = 0.05}
 
         //Menuscreen
